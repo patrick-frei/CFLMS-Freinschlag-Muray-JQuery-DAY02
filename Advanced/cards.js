ede4435282757405284acb4e2c1ddad0619e0343
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-06-10 19:51:26 +0200

    adds some cool stuff

--- Advanced/cards.js
+++ Advanced/cards.js
@@ -1,210 +1,210 @@
 let cards = [{
 	"name": "2C",
-	"id": "0",
+	"id": 0,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/69/2C.svg"
 },{
 	"name": "3C",
-	"id": "1",
+	"id": 1,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/7/70/3C.svg"
 },{
 	"name": "4C",
-	"id": "2",
+	"id": 2,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/2/25/4C.svg"
 },{
 	"name": "5C",
-	"id": "3",
+	"id": 3,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/6d/5C.svg"
 },{
 	"name": "6C",
-	"id": "4",
+	"id": 4,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/62/6C.svg"
 },{
 	"name": "7C",
-	"id": "5",
+	"id": 5,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/68/7C.svg"
 },{
 	"name": "8C",
-	"id": "6",
+	"id": 6,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/9/94/8C.svg"
 },{
 	"name": "9C",
-	"id": "7",
+	"id": 7,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/63/9C.svg"
 },{
 	"name": "10C",
-	"id": "8",
+	"id": 8,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/c/c7/10C.svg"
 },{
 	"name": "JC",
-	"id": "9",
+	"id": 9,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/1/11/JC.svg"
 },{
 	"name": "QC",
-	"id": "10",
+	"id": 10,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/9/9e/QC.svg"
 },{
 	"name": "KC",
-	"id": "11",
+	"id": 11,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/e/e1/KC.svg"
 },{
 	"name": "AC",
-	"id": "12",
+	"id": 12,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/e/eb/AC.svg"
 },{
 	"name": "2D",
-	"id": "20",
+	"id": 20,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/f/fb/2D.svg"
 },{
 	"name": "3D",
-	"id": "21",
+	"id": 21,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/5/57/3D.svg"
 },{
 	"name": "4D",
-	"id": "22",
+	"id": 22,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/c/c7/4D.svg"
 },{
 	"name": "5D",
-	"id": "23",
+	"id": 23,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/d/d9/5D.svg"
 },{
 	"name": "6D",
-	"id": "24",
+	"id": 24,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/c/cf/6D.svg"
 },{
 	"name": "7D",
-	"id": "25",
+	"id": 25,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/5/5a/7D.svg"
 },{
 	"name": "8D",
-	"id": "26",
+	"id": 26,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/8/80/8D.svg"
 },{
 	"name": "9D",
-	"id": "27",
+	"id": 27,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/0/09/9D.svg"
 },{
 	"name": "10D",
-	"id": "28",
+	"id": 28,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/8/83/10D.svg"
 },{
 	"name": "JD",
-	"id": "29",
+	"id": 29,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/3/33/JD.svg"
 },{
 	"name": "QD",
-	"id": "30",
+	"id": 30,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/63/QD.svg"
 },{
 	"name": "KD",
-	"id": "31",
+	"id": 31,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/0/06/KD.svg"
 },{
 	"name": "AD",
-	"id": "32",
+	"id": 32,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/6d/AD.svg"
 },{
 	"name": "2H",
-	"id": "40",
+	"id": 40,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/9/9d/2H.svg"
 },{
 	"name": "3H",
-	"id": "41",
+	"id": 41,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/3/3a/3H.svg"
 },{
 	"name": "4H",
-	"id": "42",
+	"id": 42,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/6f/4H.svg"
 },{
 	"name": "5H",
-	"id": "43",
+	"id": 43,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/0/03/5H.svg"
 },{
 	"name": "6H",
-	"id": "44",
+	"id": 44,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/a/a2/6H.svg"
 },{
 	"name": "7H",
-	"id": "45",
+	"id": 45,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/e/e1/7H.svg"
 },{
 	"name": "8H",
-	"id": "46",
+	"id": 46,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/3/34/8H.svg"
 },{
 	"name": "9H",
-	"id": "47",
+	"id": 47,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/e/e0/9H.svg"
 },{
 	"name": "10H",
-	"id": "48",
+	"id": 48,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/8/8a/10H.svg"
 },{
 	"name": "JH",
-	"id": "49",
+	"id": 49,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/1/15/JH.svg"
 },{
 	"name": "QH",
-	"id": "50",
+	"id": 50,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/d/d2/QH.svg"
 },{
 	"name": "KH",
-	"id": "51",
+	"id": 51,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/8/87/AH.svg"
 },{
 	"name": "AH",
-	"id": "52",
+	"id": 52,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/e/eb/AC.svg"
 },{
 	"name": "2S",
-	"id": "60",
+	"id": 60,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/d/de/2S.svg"
 },{
 	"name": "3S",
-	"id": "61",
+	"id": 61,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/c/ce/3S.svg"
 },{
 	"name": "4S",
-	"id": "62",
+	"id": 62,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/c/cc/4S.svg"
 },{
 	"name": "5S",
-	"id": "63",
+	"id": 63,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/a/ac/5S.svg"
 },{
 	"name": "6S",
-	"id": "64",
+	"id": 64,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/1/1f/6S.svg"
 },{
 	"name": "7S",
-	"id": "65",
+	"id": 65,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/a/a1/7S.svg"
 },{
 	"name": "8S",
-	"id": "66",
+	"id": 66,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/3/36/8S.svg"
 },{
 	"name": "9S",
-	"id": "67",
+	"id": 67,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/3/31/9S.svg"
 },{
 	"name": "10S",
-	"id": "68",
+	"id": 68,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/1/16/10S.svg"
 },{
 	"name": "JS",
-	"id": "69",
+	"id": 69,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/d/d9/JS.svg"
 },{
 	"name": "QS",
-	"id": "70",
+	"id": 70,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/3/35/QS.svg"
 },{
 	"name": "KS",
-	"id": "81",
+	"id": 71,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/5/5c/KS.svg"
 },{
 	"name": "AS",
-	"id": "72",
+	"id": 72,
 	"img": "https://upload.wikimedia.org/wikipedia/commons/2/27/AS.svg"
 }]
 
--- Advanced/script.js
+++ Advanced/script.js
@@ -1,14 +1,55 @@
-let playedCards = []
-let cardSuggests = []
-let j = 0
-
-$(".row_down .cardPlace").each(function(i) {
-	do
-		j = Math.floor(Math.random() * cards.length)
-	while (playedCards.includes(j) && cardSuggests.includes(j))
-	cardSuggests.push(j)
-	$(this).children("img").attr({
-		"id": cards[j].id,
-		"src": cards[j].img
+$(document).ready(function() {
+	let playedCards = [];
+	function getRdmCards() {
+		let cardSuggests = [];
+		$(".drag-area img").remove();
+		$(".drag-area").each(function(i) {
+			console.log(i);
+			console.log(cards.length);
+			if (i < cards.length) {
+				do {
+					index = Math.floor(Math.random() * cards.length);
+					id = cards[index].id;
+				} while (playedCards.includes(id) || cardSuggests.includes(id));
+				cardSuggests.push(id);
+				$(this).append("<img>");
+				$(this).children("img").attr({
+					id: id,
+					index: index,
+					class: "draggable",
+					src: cards[index].img
+				});
+				$(this).children("img").addClass("draggable");
+			}
+		});
+		console.table(cardSuggests)
+		$(".draggable").draggable({
+			containment: "window",
+			revert: "invalid"
+		})
+	}
+	$("button").on("click", getRdmCards);
+	$(".drop-area").droppable({
+		accept: function(drag) {
+			if (parseInt(drag.attr("id")) == parseInt($(this).children("img").last().attr("id")) + 1 || $(this).children("img").length == 0) {
+				return true;
+			}
+		},
+		drop: function(event, ui) {
+			$(this).append("<img>");
+			$(this).children("img").last().attr({
+				src: $(ui.draggable).attr("src"),
+				id: $(ui.draggable).attr("id")
+			});
+			$(this).children("img").last().css({
+				position: "absolute",
+				top: `${($(this).children("img").length - 1)}vh`
+			});
+			playedCards.push(parseInt($(ui.draggable).attr("id")));
+			cards.splice(cards.findIndex(card => card.id == parseInt($(ui.draggable).attr("id"))), 1);
+        	$(ui.draggable).remove();
+        	console.log(cards.length);
+        	console.log(playedCards.length);
+      	}
 	})
-})
+});
