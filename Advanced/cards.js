628b3fbc7f87ea36ae65d478969edbfef9c2b51e
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-06-10 14:52:45 +0200

    adds cards

--- Advanced/cards.js
+++ Advanced/cards.js
@@ -39,43 +39,43 @@ let cards = [{
 	"img": "https://upload.wikimedia.org/wikipedia/commons/e/eb/AC.svg"
 },{
 	"name": "2D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/6/69/2C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/f/fb/2D.svg"
 },{
 	"name": "3D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/7/70/3C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/5/57/3D.svg"
 },{
 	"name": "4D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/2/25/4C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/c/c7/4D.svg"
 },{
 	"name": "5D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/6/6d/5C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/d/d9/5D.svg"
 },{
 	"name": "6D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/6/62/6C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/c/cf/6D.svg"
 },{
 	"name": "7D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/6/68/7C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/5/5a/7D.svg"
 },{
 	"name": "8D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/9/94/8C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/8/80/8D.svg"
 },{
 	"name": "9D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/6/63/9C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/0/09/9D.svg"
 },{
 	"name": "10D",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/c/c7/10C.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/8/83/10D.svg"
 },{
 	"name": "JD",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/1/11/JC.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/3/33/JD.svg"
 },{
 	"name": "QD",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/9/9e/QC.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/6/63/QD.svg"
 },{
 	"name": "KD",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/e/e1/KC.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/0/06/KD.svg"
 },{
 	"name": "AD",
-	"img": "https://upload.wikimedia.org/wikipedia/commons/e/eb/AC.svg"
+	"img": "https://upload.wikimedia.org/wikipedia/commons/6/6d/AD.svg"
 },{
 	"name": "2H",
 	"img": "https://upload.wikimedia.org/wikipedia/commons/6/69/2C.svg"
